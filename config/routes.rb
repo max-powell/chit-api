09e2ec81409fd483579b15f7312a0d89823b5310
max-powell <github email address>
2019-06-27 15:43:50 +0100

    tweaked routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :chats
+      resources :chats, only: [:index, :create]
+      resources :messages, only: [:create]
       resources :users, only: :create
 
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
+
     end
   end
 end

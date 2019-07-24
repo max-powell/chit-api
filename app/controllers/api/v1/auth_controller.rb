a1780b89704e2d4be5607a8b354b9e50f978b2b4
max-powell <github email address>
2019-07-24 11:06:04 +0100

    removed byebugs

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,7 +5,6 @@ class Api::V1::AuthController < ApplicationController
     user = User.find_by(username: auth_params[:username])
     if user && user.authenticate(auth_params[:password])
       token = encode_token(user_id: user.id)
-      byebug
       render json: {user: user, jwt: token}
     else
       render json: {message: 'Invalid username or password'}

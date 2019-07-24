d44e56231308b9efdf9ae7097a43f9be75eb62d2
max-powell <github email address>
2019-07-24 12:22:45 +0100

    heroku debugging

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::UsersController < ApplicationController
     users = User.all.reject { |u|
       u == current_user
     }.sort_by{ |u| u[:username].downcase}
-    render json: current_user
+    render json: [current_user]
   end
 
   def create

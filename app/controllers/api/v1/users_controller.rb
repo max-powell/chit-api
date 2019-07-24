752c5125519d1989bc683092b904b0f2e331be1a
max-powell <github email address>
2019-07-24 12:05:55 +0100

    removed byebug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,8 +9,7 @@ class Api::V1::UsersController < ApplicationController
     users = User.all.reject { |u|
       u == current_user
     }.sort_by{ |u| u[:username].downcase}
-    render json: User.all
-    # , each_serializer: SenderSerializer
+    render json: users, each_serializer: SenderSerializer
   end
 
   def create

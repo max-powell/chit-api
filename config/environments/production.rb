c607b664f26ce8c9b08d28a3b4144603985d2470
max-powell <github email address>
2019-07-25 11:47:04 +0100

    config to make actioncable work with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = 'wss://chit-api.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://chit-api.herokuapp.com', 'http://chit-api.herokuapp.com']
 end

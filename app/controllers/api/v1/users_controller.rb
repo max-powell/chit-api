0761fe3bb6c899158541282cb46a98484e0d4f04
max-powell <github email address>
2019-07-25 11:03:12 +0100

    fixed user create token encoding

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.create(user_params)
     if user.valid?
-      token = encode_token(user: user)
+      token = encode_token(user_id: user.id)
       render json: {user: user, jwt: token}
     else
       render json: {error: user.errors.full_messages}

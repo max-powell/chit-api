6565d73e7b298c6bf12054cedb19342b7d94c53c
max-powell <github email address>
2019-07-30 12:12:19 +0100

    allowing localhost origin websocket upgrade

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.web_socket_server_url = 'wss://chit-api.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['https://chit-api.herokuapp.com', 'http://chit-api.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://chit-api.herokuapp.com', 'http://chit-api.herokuapp.com', 'http://localhost:3000']
 end

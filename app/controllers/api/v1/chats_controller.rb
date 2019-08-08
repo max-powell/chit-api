1e534c73e2039f4e3b701a376ffb36584a5d26f7
max-powell <github email address>
2019-08-08 14:33:59 +0100

    chat index does not send messages anymore

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ChatsController < ApplicationController
   def index
-    render json: current_user.chats, include: ['messages', 'messages.user']
+    render json: current_user.chats
   end
 
   def create
